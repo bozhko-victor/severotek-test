64e5847077f849ccab9e68a25dccf125d3026574
bozhko-victor <bozhko.v.n@yandex.ru>
2021-12-27 23:14:25 +0300

    init commit

---
+++ src/test/java/severotek/tests/TestItem.java
@@ -0,0 +1,56 @@
+package severotek.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.javafaker.Faker;
+import config.CredentialsConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestItem {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    Faker faker = new Faker();
+    String title = faker.name().title();
+    String slug = faker.internet().slug();
+    String markdown = faker.number().digits(15);
+    String text = faker.backToTheFuture().toString();
+
+    @Test
+    public void addingArticleToBlogTest() {
+        open("https://igorakintev.ru/admin/");
+        $("#id_username").setValue(credentials.login());
+        $("#id_password").setValue(credentials.password());
+        $("input[type=submit]").click();
+        $(".dashboard-title").shouldHave(Condition.text("Панель управления"));
+        $("li [href='/admin/blog/entry/add/'").click();
+        $("#content").shouldHave(Condition.text("Добавить entry"));
+        $("#id_title").setValue(title);
+        $("#id_slug").setValue(slug);
+        $("#id_text_markdown").setValue(markdown);
+        $("#id_text").setValue(text);
+        $("input.default").click();
+        open("https://igorakintev.ru/blog/");
+        $("#entries").shouldHave(Condition.text(title));
+        open("https://igorakintev.ru/admin/blog/entry/");
+        $(By.xpath("//*[@id='result_list']/tbody/tr[1]/td[1]/input")).click();
+        $(By.xpath("//select")).click();
+        $(By.xpath("//option[@value='delete_selected']")).click();
+        $(By.xpath("//button[(text()='Выполнить')]")).click();
+        $(By.xpath("//input[@value='Да, я уверен']")).click();
+    }
+
+}
